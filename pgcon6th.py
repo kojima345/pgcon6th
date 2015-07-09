b45b832cb015e2e02cddeb4695226cd0238d2f38
kojima345 <kojima@level-five.jp>
2015-07-09 12:49:59 +0900

    Create pgcon6th.py

---
+++ pgcon6th.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import sys
+stick = []
+
+L = int(sys.stdin.readline()) #1行目L
+number = int(sys.stdin.readline()) #2行目棒の数
+for i in xrange(number):
+	stick.append(int(sys.stdin.readline())) #3行目～　各棒をリストに追加
+
+answer_num = 0
+
+for a in xrange(0,number):
+	for b in xrange(a+1,number):
+		if stick[a] + stick[b] >= L:
+			break
+		for c in xrange(b+1,number):
+			if stick[a] + stick[b] + stick[c] == L:
+				answer_num += 1
+
+print('answer is %i' % answer_num)
